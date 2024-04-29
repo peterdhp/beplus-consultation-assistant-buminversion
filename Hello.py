01114f05cd353787b50bb96a3f4bbba1ab27073b
peterdhp <36303607+peterdhp@users.noreply.github.com>
2024-04-29 02:36:17 +0000

    audio_player reset

--- Hello.py
+++ Hello.py
@@ -58,6 +58,7 @@ def refresh():
     st.session_state.transcript =''
     st.session_state.temp_medical_record ="[현병력]\n\n[ROS]\n\n[신체검진]\n\n[impression]"
     st.session_state.audio = None
+    st.session_state.audio_player = None
 
 def medical_record(transcript):
     """문진 내용을 기반으로 질문을 함"""
@@ -261,7 +262,7 @@ timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
 client = OpenAI()
 if len(st.session_state.audio)>0.1:
-    st.audio(st.session_state.audio.export().read())  
+    st.audio(st.session_state.audio.export().read(),key='audio_player')  
     with st.spinner('음성 녹음을 받아적고 있습니다...'):
         asr_result = client.audio.transcriptions.create(model="whisper-1", language= "ko",prompt="이것은 의사와 환자의 진료 중 나눈 대화를 녹음한 것입니다.",file= NamedBytesIO(st.session_state.audio.export().read(), name="audio.wav"))
     st.session_state.transcript += '\n'+ asr_result.text 
