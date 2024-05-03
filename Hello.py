e6bf0b121a4ae66e1f07b868eb244f6a362bac40
peterdhp <36303607+peterdhp@users.noreply.github.com>
2024-05-03 01:02:43 +0000

    bumin and deplot merge

--- Hello.py
+++ Hello.py
@@ -6,12 +6,11 @@ import io
 import datetime
 from audiorecorder import audiorecorder
 
+
 from openai import OpenAI
 
-#st.session_state.temp_med_rec="[증상]\n[기타 특이사항]\n[진단]\n[치료, 처방 및 계획]"
-
-
-openai_api_key = st.secrets["OPENAI_API_KEY"]
+if 'disabled' not in st.session_state:
+    st.session_state.disabled = True
 
 if 'transcript_status' not in st.session_state:
     st.session_state.transcript_status=False
@@ -23,7 +22,32 @@ if "total_cost" not in st.session_state:
     st.session_state.totalcost = 0
 
 player_field=None
+bumin =True
 
+#st.session_state.temp_med_rec="[증상]\n[기타 특이사항]\n[진단]\n[치료, 처방 및 계획]"
+with st.sidebar:
+    st.image('logo.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.title("진료 도우미 (BETA ver.)")
+    st.header("사용설명서")
+    if not bumin :
+        st.subheader("0.openAI API key 입력하기")
+        st.markdown("https://m.blog.naver.com/mynameistk/223062993136 를 참고하여 APIkey를 아래에 입력한다.")
+        openai_api_key = st.text_input('OpenAI API Key', type='password')
+        if openai_api_key:
+            st.session_state.disabled = False
+    else:
+        openai_api_key = st.secrets["OPENAI_API_KEY"]
+    st.subheader("1.진료 준비하기")
+    st.markdown("`진료기록 양식`에서 원하는 양식을 선택하거나 진료기록 텍스트 상자에 원하는 양식을 붙여넣기한다.")
+    st.subheader("2.진료내용 녹음하기")
+    st.markdown("`🎙️`을 눌러준 뒤 음성 인식이 잘되는지 확인하고 진료를 진행한다.")
+    st.subheader("3.진료 마치기")
+    st.markdown("진료가 끝나면 `💾`을 누르고 음성파일을 바탕으로 진료기록이 완성되기를 기다린다.")
+    st.subheader("4.진료기록 검토하기")
+    st.markdown("`✅ impression list 및 진료 내용 검토`을 눌러 진료기록이 검토되기를 기다린다.")
+    st.subheader("5.새로고침")
+    st.markdown("`🔄 새로운 환자`을 눌러 이전 진료기록을 지운다.")
+    
 def refresh():
     st.session_state.totalcost = 0
     st.session_state.format_type = '기본'
@@ -34,8 +58,7 @@ def refresh():
     if player_field is not None:
         player_field.empty()
 
-
-def medical_record(transcript):
+def medical_record(transcript,openai_api_key):
     prompt_template = """Given the transcript, write a semi-filled medical report of the patient. 
 write the medical record based ONLY on the information given. If you don't have enough information to complete the medical record, just leave it blank.
 After the medical record, give the list of things that the doctor explained to the patient during the consulaltation.
@@ -69,7 +92,7 @@ CBC 시행
 """
 
     prompt = PromptTemplate.from_template(prompt_template)
-    llm = ChatOpenAI(model_name="gpt-4-turbo", temperature = 0)
+    llm = ChatOpenAI(model_name="gpt-4-turbo", temperature = 0,api_key=openai_api_key)
     output_parser = StrOutputParser()
 
     chain = prompt | llm | output_parser    
@@ -77,12 +100,13 @@ CBC 시행
     output = chain.invoke({"transcript" : transcript})
     return output
 
-def medical_record_voicecomplete():
+def medical_record_voicecomplete(openai_api_key): 
+    
     prompt_template = """Given a transcript of a patient consultation and a incomplete medical record, complete and edit the medical record. 
-Complete or edit the medical record based ONLY on the information given. If you don't have enough information to complete the medical record, just leave it blank.
+Complete or edit the medical record based ONLY on the information given. If you don't have enough information to complete the medical record, LEAVE IT BLANK.
 For the physical examination KEEP THE FORMAT and only change what is necessary.
 DON'T give the impression list. After the medical record, give the list of things that the doctor explained to the patient during the consultation. the title should be [환자 설명 내용]
-Use Korean.
+Use Korean with sentences ending in '음, 함 etc.'.
 
 [transcript]
 {transcript}
@@ -97,7 +121,7 @@ Use Korean.
     
     prompt = PromptTemplate.from_template(prompt_template)
     
-    llm = ChatOpenAI(model_name="gpt-4-turbo", temperature = 0)
+    llm = ChatOpenAI(model_name="gpt-4-turbo", temperature = 0,api_key=openai_api_key)
     output_parser = StrOutputParser()
 
     chain = prompt | llm | output_parser    
@@ -107,9 +131,9 @@ Use Korean.
 def update_text():
     if st.session_state.format_type == '없음' and st.session_state.temp_medical_record == "":
         with st.spinner('음성 녹음을 바탕으로 진료 기록을 완성하고 있습니다...'):
-            st.session_state.LLM_medrecord = medical_record(transcript=st.session_state.transcript)
+            st.session_state.LLM_medrecord = medical_record(transcript=st.session_state.transcript,openai_api_key=openai_api_key)
     else :    
-        chain = medical_record_voicecomplete()
+        chain = medical_record_voicecomplete(openai_api_key)
         with st.spinner('음성 녹음을 바탕으로 진료 기록을 완성하고 있습니다...'):
             st.session_state.LLM_medrecord = chain.invoke({"transcript" : st.session_state.transcript, "incomplete_medrec" : st.session_state.temp_medical_record})
     st.session_state.temp_medical_record = st.session_state.LLM_medrecord 
@@ -117,21 +141,29 @@ def update_text():
 def update_text_advise():
     if st.session_state.format_type == '없음' and st.session_state.temp_medical_record == "":
         with st.spinner('음성 녹음을 바탕으로 진료 기록을 완성하고 있습니다...'):
-            st.session_state.LLM_medrecord = medical_record(transcript=st.session_state.transcript)
+            st.session_state.LLM_medrecord = medical_record(transcript=st.session_state.transcript,openai_api_key=openai_api_key)
     else :    
-        chain = medical_record_voicecomplete()
+        chain = medical_record_voicecomplete(openai_api_key)
         with st.spinner('음성 녹음을 바탕으로 진료 기록을 완성하고 있습니다...'):
             st.session_state.LLM_medrecord = chain.invoke({"transcript" : st.session_state.transcript, "incomplete_medrec" : st.session_state.temp_medical_record})
     st.session_state.temp_medical_record = st.session_state.LLM_medrecord 
     st.success("진료 기록을 성공적으로 완성하였습니다.")
     with st.spinner('진료 내용을 검토하고 있습니다...'):
-        output = medical_advisor(st.session_state.temp_medical_record,st.session_state.transcript)
+        output = medical_advisor(st.session_state.temp_medical_record,st.session_state.transcript,openai_api_key=openai_api_key)
     st.session_state.temp_medical_record += '\n\n'+ output
     st.success("진료 내용 검토 성공적으로 완료 되었습니다.")
- 
+
+
+def advise(): 
+    with st.spinner('진료 기록을 검토 및 추정진단을 뽑고 있습니다...'):
+        output = medical_advisor(st.session_state.temp_medical_record_2,st.session_state.transcript)
+    #print(output)
+    st.session_state.temp_medical_record_2 += '\n\n'+ output
+    st.success("진료 내용 검토 성공적으로 완료 되었습니다.")
+
 def recorddemo():
     st.session_state.transcript = "오늘 어디 아파서 오셨어요? 선생님 제가 최근 며칠부터 너무 죽을 것 같아서요. 어제는 오늘부터 막 토도 하고 지금 기운도 너무 없고 음식도 못 먹겠고 지금 계속 토하고 배 아프고 너무 힘들어요. 그래요? 토하기 시작한 건 언제였어요? 토한 건 어제 오후부터 속이 안 좋더니 오늘부터는 계속 토하고 그래요. 토를 몇 번 하셨어요? 글쎄요. 먹은 것도 없이 계속 나왔어요. 토하면 먹은 게 아니라 그냥 우유 같은 거만 나오네요. 마지막으로 식사하신 건 언젠데요? 식사는 조금씩 했어요. 조금씩. 죽 같은 거 그냥. 마지막으로 언제 식사하셨어요? 아침에도 좀 줘 먹어야겠다 싶어서 너무 지금 기운도 하나도 없고 지금 너무 힘들어요. 설사는 하셨고요? 설사는 그냥 변이 좀 없다 정도만 했었고 마지막으로 본 건 언젠데요? 그건 어제인가 그젠가 배가 아프진 않으세요? 배가 아파요. 어디가 아파요? 배꼽 주변 다 전체적으로 아파요. 전체적으로 다? 배 아픈 것도 그럼 어제부터 그러신 거예요? 배는 요 근래부터 조금씩 조금씩 아프다니 어제 그제부터 좀 더 아파요. 근래? 근래면 정확히 어느 정도 됐을까요? 글쎄요. 제가 요새 좀 컨디션이 안 좋다 싶더니 갑자기 이게 심해지네요. 요새가 어느 정도 되셨어요? 글쎄요. 한 이번 주? 이번 주? 그럼 일주일 정도? 이번 주 한 글쎄요. 한 3-4일 됐을까? 3-4일? 그냥 제가 좀 몸살 기운 나고 좀 기침하고 좀 감기 기운이 있더니 컨디션이 확 너무 안 좋아지네요. 감기 기운이 있다가 안 좋아지셨어요? 네. 확 안 좋아지네요. 갑자기. 근데 변은 그냥 계속 묽게만 하고 네. 변은 그냥 섞여나오는 건 아니고요. 열은? 열은 그렇게 안 납니다. 열은 없고요. 원래 앓고 계시는 병 있으세요? 뭐 딱히 앓고 있는 병은 없어요. 그냥 가끔 정기적으로 먹는 약 같은 거? 제가 진통제는 좀 자주 먹어요. 허리가 너무 아파요. 허리가 아파요? 허리는 언제부터 그랬는데? 허리는 좀 술창이 됐죠. 얼마나? 몇 년 된 것 같아요. 몇 년? 그럼 진통제는 뭐 계속 먹어야 돼요? 아니면은 계속 먹어야 돼요. 진짜 계속 먹어야 돼요. 너무 아파서 그거 어디서 처방 받으신 거예요? 처음 봐서 먹죠. 진통제는 이름은 모르시죠? 이름은 잘 모르시고. 이름은 잘 모르시고. 처방은 어디서 받으세요? 동네 정형외과. 동네 정형외과. 아침에 너무 힘들어요. 선생님. 일단 네. 아침에 너무 힘들어요. 지금. 너무 기분도 없고. X-ray랑 혈액 검사를 좀 잠깐 하고 그리고 제가 좀 보도록 하겠습니다."
-
+ 
 def format_retriever(format_type):
     
     format_lib ={}
@@ -157,14 +189,8 @@ Rt. extension/flexion = 50/150
 def call_format():
     st.session_state.temp_medical_record = format_retriever(st.session_state.format_type)
 
-def advise(): 
-    with st.spinner('진료 기록을 검토 및 추정진단을 뽑고 있습니다...'):
-        output = medical_advisor(st.session_state.temp_medical_record,st.session_state.transcript)
-    #print(output)
-    st.session_state.temp_medical_record = st.session_state.LLM_medrecord + '\n\n'+ output
-    st.success("진료 내용 검토 성공적으로 완료 되었습니다.")
 
-def medical_advisor(medical_record, transcript):
+def medical_advisor(medical_record, transcript,openai_api_key):
     prompt_template = """Let's say you are a medical school professor.
 Given a transcript of a patient consultation and a complete medical record written, 
 Give a list of impression in the format below :
@@ -187,7 +213,7 @@ Be as brief and clear as possible, no longer than 50 Korean characters.
 
     prompt = PromptTemplate.from_template(prompt_template)
     
-    llm = ChatOpenAI(model_name="gpt-4-turbo", temperature = 0)
+    llm = ChatOpenAI(model_name="gpt-4-turbo", temperature = 0,api_key=openai_api_key)
     output_parser = StrOutputParser()
 
     chain = prompt | llm | output_parser
@@ -203,63 +229,58 @@ class NamedBytesIO(io.BytesIO):
 st.selectbox("진료기록 양식", options=['없음', '기본', '어깨통증'],index=1,on_change=call_format, key='format_type')
 
 medical_record_area = st.empty()
-medical_record_area.text_area('진료 기록', value="[현병력]\n\n[ROS]", height=600, key='temp_medical_record')
+if not st.session_state.transcript_status:
+    medical_record_area.text_area('진료 기록', value="[현병력]\n\n[ROS]", height=600, key='temp_medical_record')
 
-st.session_state.audio=audiorecorder(start_prompt="", stop_prompt="", pause_prompt="", key='recordings')
-timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+#timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-#st.write(st.session_state)
-
+#byte_io = io.BytesIO()
+#audio.export(byte_io, format='mp3')
+#byte_io.seek(0)
 thirty_minutes = 30 * 60 * 1000
-if len(st.session_state.audio)>thirty_minutes:
-    st.warning('음성 녹음은 30분을 초과할 수 없습니다. 첫 30분에 대한 진료내용만 사용합니다.', icon='⚠')
-    st.session_state.audio = st.session_state.audio[:thirty_minutes]
 
 
-client = OpenAI()
-if st.session_state.recordings and len(st.session_state.audio)>100:
+if not bumin and not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+
+if bumin or openai_api_key.startswith('sk-'):
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.audio=audiorecorder(start_prompt="", stop_prompt="", pause_prompt="", key='recordings')
+    
+    if len(st.session_state.audio)>thirty_minutes:
+        st.warning('음성 녹음은 30분을 초과할 수 없습니다. 첫 30분에 대한 진료내용만 사용합니다.', icon='⚠')
+        st.session_state.audio = st.session_state.audio[:thirty_minutes]
+if openai_api_key.startswith('sk-') and st.session_state.recordings and len(st.session_state.audio)>100:
+    #st.write(len(st.session_state.audio))
     player_field = st.audio(st.session_state.audio.export().read())  
     if not st.session_state.transcript_status :
         with st.spinner('음성 녹음을 받아적고 있습니다...'):
             asr_result = client.audio.transcriptions.create(model="whisper-1", language= "ko",file= NamedBytesIO(st.session_state.audio.export().read(), name="audio.wav"))
         st.session_state.transcript += '\n'+ asr_result.text 
         st.session_state.transcript_status = True
-
+        
+        #st.text_area("진료 음성기록", value =st.session_state.transcript, key='transcript')
         if st.session_state.format_type == '없음' and st.session_state.temp_medical_record == "":
             with st.spinner('음성 녹음을 바탕으로 진료 기록을 완성하고 있습니다...'):
-                st.session_state.LLM_medrecord = medical_record(transcript=st.session_state.transcript)
+                st.session_state.LLM_medrecord = medical_record(transcript=st.session_state.transcript,openai_api_key=openai_api_key)
         else :    
-            chain = medical_record_voicecomplete()
+            chain = medical_record_voicecomplete(openai_api_key)
             with st.spinner('음성 녹음을 바탕으로 진료 기록을 완성하고 있습니다...'):
                 st.session_state.LLM_medrecord = chain.invoke({"transcript" : st.session_state.transcript, "incomplete_medrec" : st.session_state.temp_medical_record})
-        medical_record_area.empty()
-        medical_record_area.text_area('진료 기록', value=st.session_state.LLM_medrecord , height=600)
-        
 
-#st.text_area("진료 음성기록", key='transcript')
+if st.session_state.transcript_status :
+    medical_record_area.text_area('진료 기록', value=st.session_state.LLM_medrecord , height=600, key= 'temp_medical_record_2')
+        
+#st.write(st.session_state)
 #st.button('✍🏻 진료기록 자동 완성 ',on_click=update_text)
-st.button('✅ impression list 및 진료 내용 검토',on_click=advise)
+st.button('✅ impression list 및 진료 내용 검토',on_click=advise, disabled=not st.session_state.transcript_status)
 st.button('🔄 새로운 환자',on_click=refresh,key='refreshbutton')
    
 #encoded_image = base64.b64encode(open("logo.png", "rb").read()).decode()
 
 
-with st.sidebar:
-    #st.write(st.session_state)
-    st.image('logo.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-    st.title("진료 도우미 (BETA ver.)")
-    st.header("사용설명서")
-    st.subheader("1.진료 준비하기")
-    st.markdown("`진료기록 양식`에서 원하는 양식을 선택하거나 진료기록 텍스트 상자에 원하는 양식을 붙여넣기한다.")
-    st.subheader("2.진료내용 녹음하기")
-    st.markdown("`🎙️`을 눌러준 뒤 음성 인식이 잘되는지 확인하고 진료를 진행한다.")
-    st.subheader("3.진료 마치기")
-    st.markdown("진료가 끝나면 `💾`을 누르고 음성파일을 바탕으로 진료기록이 완성되기를 기다린다.")
-    st.subheader("4.진료기록 검토하기")
-    st.markdown("`✅ impression list 및 진료 내용 검토`을 눌러 진료기록이 검토되기를 기다린다.")
-    st.subheader("5.새로고침")
-    st.markdown("`🔄 새로운 환자`을 눌러 이전 진료기록을 지운다.")
+
     
-    #st.button("음성녹음 Demo",on_click=recorddemo)
+#st.button("음성녹음 Demo",on_click=recorddemo)
     #st.button("자동작성완료 Demo",on_click=completedemo)
     #st.session_state
